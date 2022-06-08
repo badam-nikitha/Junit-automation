3c20b32fd70ed5d627b838a7c168e7f4ea8a274e
Badam Nikitha <badam.nikitha@covalensedigital.net>
2022-06-08 10:29:57 +0530

    adding Junit

---
+++ jenkinsfile.groovy
@@ -0,0 +1,36 @@
+pipeline {
+
+    agent any
+    stages {
+
+        stage('Checkout Codebase'){
+            steps{
+                cleanWs()
+                checkout scm: [$class: 'GitSCM', branches: [[name: '*/master']],userRemoteConfigs:
+                [[credentialsId: 'ssh-key', url: 'git@github.com:badam-nikitha/Junit-automation.git']]]
+            }
+        }
+
+        stage('Build'){
+            steps{
+                bat 'mkdir lib'
+                bat 'cd lib/ ; wget https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.7.0/junit-platform-console-standalone-1.7.0-all.jar'
+                bat 'cd src ; javac -cp "../lib/junit-platform-console-standalone-1.7.0-all.jar" CarTest.java Car.java App.java'
+            }
+        }
+
+        stage('Test'){
+            steps{
+                bat 'cd src/ ; java -jar ../lib/junit-platform-console-standalone-1.7.0-all.jar -cp "." --select-class CarTest --reports-dir="reports"'
+                junit 'src/reports/*-jupiter.xml'
+            }
+        }
+
+        stage('Deploy'){
+            steps{
+                bat 'cd src/ ; java App' 
+            }
+        }
+    }
+
+}
